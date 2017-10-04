1cc0a76c13c532e8eda2b465a5a5ee98acddfce1
sameersayani@gmail.com <sameersayani@gmail.com>
2017-10-04 15:50:11 +0530

    check in stuff

---
+++ Src/JustBlog_SourceCode/JustBlog/src/JustBlog/JustBlog.Core/Mappings/CategoryMap.cs
@@ -0,0 +1,18 @@
+﻿
+using FluentNHibernate.Mapping;
+using ObjectRef.Core.Objects;
+
+namespace ObjectRef.Core.Mappings
+{
+  public class CategoryMap : ClassMap<Category>
+  {
+    public CategoryMap()
+    {
+      Id(x => x.Id);
+      Map(x => x.Name).Length(50).Not.Nullable();
+      Map(x => x.UrlSlug).Length(50).Not.Nullable();
+      Map(x => x.Description).Length(200);
+      HasMany(x => x.Posts).Inverse().Cascade.All().KeyColumn("Category");
+    }
+  }
+}
---
+++ Src/JustBlog_SourceCode/JustBlog/src/JustBlog/JustBlog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ObjectRef.MvcApplication" Language="C#" %>
